6ba503dc59e56eda444ea3079c7acf6822c372ef
Erross <33665337+Erross@users.noreply.github.com>
2026-08-22 18:25:52 -0500

    Wire Grade 4 Math diversity expansion

--- js/banks.js
+++ js/banks.js
@@ -7,9 +7,14 @@ import { BANK_G3_ELA_EXPANSION } from "../data/grade-3/ela-expansion.js";
 import { BANK_G3_ELA_EXPANSION_2 } from "../data/grade-3/ela-expansion-2.js";
 import { BANK_G4_MATH } from "../data/grade-4/math.js";
 import { BANK_G4_MATH_EXPANSION } from "../data/grade-4/math-expansion.js";
+import { BANK_G4_MATH_BLUEPRINT_EXPANSION } from "../data/grade-4/math-blueprint-expansion.js";
+import { BANK_G4_MATH_DIVERSITY_NBT } from "../data/grade-4/math-diversity-nbt.js";
+import { BANK_G4_MATH_DIVERSITY_NF } from "../data/grade-4/math-diversity-nf.js";
+import { BANK_G4_MATH_DIVERSITY_RA } from "../data/grade-4/math-diversity-ra.js";
+import { BANK_G4_MATH_DIVERSITY_GM } from "../data/grade-4/math-diversity-gm.js";
+import { BANK_G4_MATH_DIVERSITY_DS } from "../data/grade-4/math-diversity-ds.js";
 import { BANK_G4_MATH_PE_EXPANSION } from "../data/grade-4/math-pe-expansion.js";
 import { BANK_G4_MATH_OPERATIONAL_PE } from "../data/grade-4/math-operational-pe.js";
-import { BANK_G4_MATH_BLUEPRINT_EXPANSION } from "../data/grade-4/math-blueprint-expansion.js";
 import { BANK_G4_ELA } from "../data/grade-4/ela.js";
 import { BANK_G4_ELA_EXPANSION } from "../data/grade-4/ela-expansion.js";
 import { BANK_G4_ELA_EXPANSION_2 } from "../data/grade-4/ela-expansion-2.js";
@@ -32,37 +37,37 @@ import { BANK_G5_SCIENCE_EXPANSION_2 } from "../data/grade-5/science-expansion-2
 import { BANK_G5_SCIENCE_BLUEPRINT_EXPANSION } from "../data/grade-5/science-blueprint-expansion.js";
 import { BANK_G6_MATH } from "../data/grade-6/math.js";
 import { BANK_G6_MATH_EXPANSION } from "../data/grade-6/math-expansion.js";
-import { BANK_G6_MATH_PE_EXPANSION } from "../data/grade-6/math-pe-expansion.js";
-import { BANK_G6_MATH_OPERATIONAL_PE } from "../data/grade-6/math-operational-pe.js";
 import { BANK_G6_MATH_BLUEPRINT_EXPANSION } from "../data/grade-6/math-blueprint-expansion.js";
 import { BANK_G6_MATH_DIVERSITY_RP } from "../data/grade-6/math-diversity-rp.js";
 import { BANK_G6_MATH_DIVERSITY_NS } from "../data/grade-6/math-diversity-ns.js";
 import { BANK_G6_MATH_DIVERSITY_EEI } from "../data/grade-6/math-diversity-eei.js";
 import { BANK_G6_MATH_DIVERSITY_GM_DSP } from "../data/grade-6/math-diversity-gm-dsp.js";
+import { BANK_G6_MATH_PE_EXPANSION } from "../data/grade-6/math-pe-expansion.js";
+import { BANK_G6_MATH_OPERATIONAL_PE } from "../data/grade-6/math-operational-pe.js";
 import { BANK_G6_ELA } from "../data/grade-6/ela.js";
 import { BANK_G6_ELA_EXPANSION } from "../data/grade-6/ela-expansion.js";
 import { BANK_G7_MATH } from "../data/grade-7/math.js";
 import { BANK_G7_MATH_EXPANSION } from "../data/grade-7/math-expansion.js";
-import { BANK_G7_MATH_PE_EXPANSION } from "../data/grade-7/math-pe-expansion.js";
-import { BANK_G7_MATH_OPERATIONAL_PE } from "../data/grade-7/math-operational-pe.js";
 import { BANK_G7_MATH_BLUEPRINT_EXPANSION } from "../data/grade-7/math-blueprint-expansion.js";
 import { BANK_G7_MATH_DIVERSITY_RP } from "../data/grade-7/math-diversity-rp.js";
 import { BANK_G7_MATH_DIVERSITY_NS } from "../data/grade-7/math-diversity-ns.js";
 import { BANK_G7_MATH_DIVERSITY_EEI } from "../data/grade-7/math-diversity-eei.js";
 import { BANK_G7_MATH_DIVERSITY_GM } from "../data/grade-7/math-diversity-gm.js";
 import { BANK_G7_MATH_DIVERSITY_DSP } from "../data/grade-7/math-diversity-dsp.js";
+import { BANK_G7_MATH_PE_EXPANSION } from "../data/grade-7/math-pe-expansion.js";
+import { BANK_G7_MATH_OPERATIONAL_PE } from "../data/grade-7/math-operational-pe.js";
 import { BANK_G7_ELA } from "../data/grade-7/ela.js";
 import { BANK_G7_ELA_EXPANSION } from "../data/grade-7/ela-expansion.js";
 import { BANK_G8_MATH } from "../data/grade-8/math.js";
 import { BANK_G8_MATH_EXPANSION } from "../data/grade-8/math-expansion.js";
-import { BANK_G8_MATH_PE_EXPANSION } from "../data/grade-8/math-pe-expansion.js";
-import { BANK_G8_MATH_OPERATIONAL_PE } from "../data/grade-8/math-operational-pe.js";
 import { BANK_G8_MATH_BLUEPRINT_EXPANSION } from "../data/grade-8/math-blueprint-expansion.js";
 import { BANK_G8_MATH_DIVERSITY_EEI } from "../data/grade-8/math-diversity-eei.js";
 import { BANK_G8_MATH_DIVERSITY_NS } from "../data/grade-8/math-diversity-ns.js";
 import { BANK_G8_MATH_DIVERSITY_GM } from "../data/grade-8/math-diversity-gm.js";
 import { BANK_G8_MATH_DIVERSITY_DSP } from "../data/grade-8/math-diversity-dsp.js";
 import { BANK_G8_MATH_DIVERSITY_FUNCTIONS } from "../data/grade-8/math-diversity-functions.js";
+import { BANK_G8_MATH_PE_EXPANSION } from "../data/grade-8/math-pe-expansion.js";
+import { BANK_G8_MATH_OPERATIONAL_PE } from "../data/grade-8/math-operational-pe.js";
 import { BANK_G8_ELA } from "../data/grade-8/ela.js";
 import { BANK_G8_ELA_EXPANSION } from "../data/grade-8/ela-expansion.js";
 import { BANK_G8_ELA_EXPANSION_2 } from "../data/grade-8/ela-expansion-2.js";
@@ -73,7 +78,7 @@ import { BANK_G8_SCIENCE_BLUEPRINT_EXPANSION } from "../data/grade-8/science-blu
 
 export const BANKS=Object.freeze({
   "g3-math":[...BANK_G3_MATH,...BANK_G3_MATH_EXPANSION,...BANK_G3_MATH_BLUEPRINT_EXPANSION,...BANK_G3_MATH_OPERATIONAL_PE],"g3-ela":[...BANK_G3_ELA,...BANK_G3_ELA_EXPANSION,...BANK_G3_ELA_EXPANSION_2],
-  "g4-math":[...BANK_G4_MATH,...BANK_G4_MATH_EXPANSION,...BANK_G4_MATH_BLUEPRINT_EXPANSION,...BANK_G4_MATH_PE_EXPANSION,...BANK_G4_MATH_OPERATIONAL_PE],"g4-ela":[...BANK_G4_ELA,...BANK_G4_ELA_EXPANSION,...BANK_G4_ELA_EXPANSION_2,...BANK_G4_ELA_EXPANSION_3],
+  "g4-math":[...BANK_G4_MATH,...BANK_G4_MATH_EXPANSION,...BANK_G4_MATH_BLUEPRINT_EXPANSION,...BANK_G4_MATH_DIVERSITY_NBT,...BANK_G4_MATH_DIVERSITY_NF,...BANK_G4_MATH_DIVERSITY_RA,...BANK_G4_MATH_DIVERSITY_GM,...BANK_G4_MATH_DIVERSITY_DS,...BANK_G4_MATH_PE_EXPANSION,...BANK_G4_MATH_OPERATIONAL_PE],"g4-ela":[...BANK_G4_ELA,...BANK_G4_ELA_EXPANSION,...BANK_G4_ELA_EXPANSION_2,...BANK_G4_ELA_EXPANSION_3],
   "g5-math":[...BANK_G5_MATH,...BANK_G5_MATH_EXPANSION,...BANK_G5_MATH_FEASIBILITY_EXPANSION,...BANK_G5_MATH_DIVERSITY_NBT,...BANK_G5_MATH_DIVERSITY_NF,...BANK_G5_MATH_DIVERSITY_RA,...BANK_G5_MATH_DIVERSITY_GM,...BANK_G5_MATH_DIVERSITY_DS,...BANK_G5_MATH_OPERATIONAL_PE],"g5-ela":[...BANK_G5_ELA,...BANK_G5_ELA_EXPANSION,...BANK_G5_ELA_EXPANSION_2],"g5-science":[...BANK_G5_SCIENCE,...BANK_G5_SCIENCE_EXPANSION,...BANK_G5_SCIENCE_EXPANSION_2,...BANK_G5_SCIENCE_BLUEPRINT_EXPANSION],
   "g6-math":[...BANK_G6_MATH,...BANK_G6_MATH_EXPANSION,...BANK_G6_MATH_BLUEPRINT_EXPANSION,...BANK_G6_MATH_DIVERSITY_RP,...BANK_G6_MATH_DIVERSITY_NS,...BANK_G6_MATH_DIVERSITY_EEI,...BANK_G6_MATH_DIVERSITY_GM_DSP,...BANK_G6_MATH_PE_EXPANSION,...BANK_G6_MATH_OPERATIONAL_PE],"g6-ela":[...BANK_G6_ELA,...BANK_G6_ELA_EXPANSION],
   "g7-math":[...BANK_G7_MATH,...BANK_G7_MATH_EXPANSION,...BANK_G7_MATH_BLUEPRINT_EXPANSION,...BANK_G7_MATH_DIVERSITY_RP,...BANK_G7_MATH_DIVERSITY_NS,...BANK_G7_MATH_DIVERSITY_EEI,...BANK_G7_MATH_DIVERSITY_GM,...BANK_G7_MATH_DIVERSITY_DSP,...BANK_G7_MATH_PE_EXPANSION,...BANK_G7_MATH_OPERATIONAL_PE],"g7-ela":[...BANK_G7_ELA,...BANK_G7_ELA_EXPANSION],

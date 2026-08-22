7e7d3963f99ae4c46f4c71c46389b706e22ab9a4
Erross <33665337+Erross@users.noreply.github.com>
2026-08-22 15:27:47 -0500

    Wire Grade 4 Math expansion banks

--- js/banks.js
+++ js/banks.js
@@ -2,6 +2,8 @@ import { BANK_G3_MATH } from "../data/grade-3/math.js";
 import { BANK_G3_MATH_EXPANSION } from "../data/grade-3/math-expansion.js";
 import { BANK_G3_ELA } from "../data/grade-3/ela.js";
 import { BANK_G4_MATH } from "../data/grade-4/math.js";
+import { BANK_G4_MATH_EXPANSION } from "../data/grade-4/math-expansion.js";
+import { BANK_G4_MATH_PE_EXPANSION } from "../data/grade-4/math-pe-expansion.js";
 import { BANK_G4_ELA } from "../data/grade-4/ela.js";
 import { BANK_G4_ELA_EXPANSION } from "../data/grade-4/ela-expansion.js";
 import { BANK_G5_MATH } from "../data/grade-5/math.js";
@@ -20,7 +22,7 @@ import { BANK_G8_SCIENCE_EXPANSION } from "../data/grade-8/science-expansion.js"
 
 export const BANKS=Object.freeze({
   "g3-math":[...BANK_G3_MATH,...BANK_G3_MATH_EXPANSION],"g3-ela":BANK_G3_ELA,
-  "g4-math":BANK_G4_MATH,"g4-ela":[...BANK_G4_ELA,...BANK_G4_ELA_EXPANSION],
+  "g4-math":[...BANK_G4_MATH,...BANK_G4_MATH_EXPANSION,...BANK_G4_MATH_PE_EXPANSION],"g4-ela":[...BANK_G4_ELA,...BANK_G4_ELA_EXPANSION],
   "g5-math":[...BANK_G5_MATH,...BANK_G5_MATH_EXPANSION],"g5-ela":BANK_G5_ELA,"g5-science":[...BANK_G5_SCIENCE,...BANK_G5_SCIENCE_EXPANSION],
   "g6-math":BANK_G6_MATH,"g6-ela":BANK_G6_ELA,
   "g7-math":BANK_G7_MATH,"g7-ela":BANK_G7_ELA,

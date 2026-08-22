19fb2844074f8dade52106fb5693b75c0eb3b16e
Erross <33665337+Erross@users.noreply.github.com>
2026-08-22 08:44:35 -0500

    Add Grade 5 auto-scorable development vertical slice

--- js/banks.js
+++ js/banks.js
@@ -1,6 +1,12 @@
+import { BANK_G5_MATH } from "../data/grade-5/math.js";
+import { BANK_G5_ELA } from "../data/grade-5/ela.js";
+import { BANK_G5_SCIENCE } from "../data/grade-5/science.js";
 import { BANK_G8_MATH } from "../data/grade-8/math.js";
 import { BANK_G8_ELA } from "../data/grade-8/ela.js";
 import { BANK_G8_SCIENCE } from "../data/grade-8/science.js";
 
-export const BANKS=Object.freeze({"g8-math":BANK_G8_MATH,"g8-ela":BANK_G8_ELA,"g8-science":BANK_G8_SCIENCE});
+export const BANKS=Object.freeze({
+  "g5-math":BANK_G5_MATH,"g5-ela":BANK_G5_ELA,"g5-science":BANK_G5_SCIENCE,
+  "g8-math":BANK_G8_MATH,"g8-ela":BANK_G8_ELA,"g8-science":BANK_G8_SCIENCE
+});
 export const getBank=id=>BANKS[id]||[];

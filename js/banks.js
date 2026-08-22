a1745431a4bd167e9de50d1e509004650cf365c8
Erross <33665337+Erross@users.noreply.github.com>
2026-08-22 13:52:56 -0500

    Aggregate Grade 3 Math expansion into effective bank

--- js/banks.js
+++ js/banks.js
@@ -1,4 +1,5 @@
 import { BANK_G3_MATH } from "../data/grade-3/math.js";
+import { BANK_G3_MATH_EXPANSION } from "../data/grade-3/math-expansion.js";
 import { BANK_G3_ELA } from "../data/grade-3/ela.js";
 import { BANK_G4_MATH } from "../data/grade-4/math.js";
 import { BANK_G4_ELA } from "../data/grade-4/ela.js";
@@ -18,7 +19,7 @@ import { BANK_G8_SCIENCE } from "../data/grade-8/science.js";
 import { BANK_G8_SCIENCE_EXPANSION } from "../data/grade-8/science-expansion.js";
 
 export const BANKS=Object.freeze({
-  "g3-math":BANK_G3_MATH,"g3-ela":BANK_G3_ELA,
+  "g3-math":[...BANK_G3_MATH,...BANK_G3_MATH_EXPANSION],"g3-ela":BANK_G3_ELA,
   "g4-math":BANK_G4_MATH,"g4-ela":[...BANK_G4_ELA,...BANK_G4_ELA_EXPANSION],
   "g5-math":[...BANK_G5_MATH,...BANK_G5_MATH_EXPANSION],"g5-ela":BANK_G5_ELA,"g5-science":[...BANK_G5_SCIENCE,...BANK_G5_SCIENCE_EXPANSION],
   "g6-math":BANK_G6_MATH,"g6-ela":BANK_G6_ELA,

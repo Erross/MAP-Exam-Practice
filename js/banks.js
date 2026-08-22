6964b5c1a753c1dc4b985ec13b3544374d596211
Erross <33665337+Erross@users.noreply.github.com>
2026-08-22 12:36:31 -0500

    Wire Grade 3 MAP development banks

--- js/banks.js
+++ js/banks.js
@@ -1,3 +1,5 @@
+import { BANK_G3_MATH } from "../data/grade-3/math.js";
+import { BANK_G3_ELA } from "../data/grade-3/ela.js";
 import { BANK_G5_MATH } from "../data/grade-5/math.js";
 import { BANK_G5_ELA } from "../data/grade-5/ela.js";
 import { BANK_G5_SCIENCE } from "../data/grade-5/science.js";
@@ -6,6 +8,7 @@ import { BANK_G8_ELA } from "../data/grade-8/ela.js";
 import { BANK_G8_SCIENCE } from "../data/grade-8/science.js";
 
 export const BANKS=Object.freeze({
+  "g3-math":BANK_G3_MATH,"g3-ela":BANK_G3_ELA,
   "g5-math":BANK_G5_MATH,"g5-ela":BANK_G5_ELA,"g5-science":BANK_G5_SCIENCE,
   "g8-math":BANK_G8_MATH,"g8-ela":BANK_G8_ELA,"g8-science":BANK_G8_SCIENCE
 });

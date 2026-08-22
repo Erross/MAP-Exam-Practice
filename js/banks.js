5313c38f85003620f019a08b2aa3f120d6b4e102
Erross <33665337+Erross@users.noreply.github.com>
2026-08-22 12:43:55 -0500

    Wire Grade 6 and Grade 7 MAP development banks

--- js/banks.js
+++ js/banks.js
@@ -5,6 +5,10 @@ import { BANK_G4_ELA } from "../data/grade-4/ela.js";
 import { BANK_G5_MATH } from "../data/grade-5/math.js";
 import { BANK_G5_ELA } from "../data/grade-5/ela.js";
 import { BANK_G5_SCIENCE } from "../data/grade-5/science.js";
+import { BANK_G6_MATH } from "../data/grade-6/math.js";
+import { BANK_G6_ELA } from "../data/grade-6/ela.js";
+import { BANK_G7_MATH } from "../data/grade-7/math.js";
+import { BANK_G7_ELA } from "../data/grade-7/ela.js";
 import { BANK_G8_MATH } from "../data/grade-8/math.js";
 import { BANK_G8_ELA } from "../data/grade-8/ela.js";
 import { BANK_G8_SCIENCE } from "../data/grade-8/science.js";
@@ -13,6 +17,8 @@ export const BANKS=Object.freeze({
   "g3-math":BANK_G3_MATH,"g3-ela":BANK_G3_ELA,
   "g4-math":BANK_G4_MATH,"g4-ela":BANK_G4_ELA,
   "g5-math":BANK_G5_MATH,"g5-ela":BANK_G5_ELA,"g5-science":BANK_G5_SCIENCE,
+  "g6-math":BANK_G6_MATH,"g6-ela":BANK_G6_ELA,
+  "g7-math":BANK_G7_MATH,"g7-ela":BANK_G7_ELA,
   "g8-math":BANK_G8_MATH,"g8-ela":BANK_G8_ELA,"g8-science":BANK_G8_SCIENCE
 });
 export const getBank=id=>BANKS[id]||[];
